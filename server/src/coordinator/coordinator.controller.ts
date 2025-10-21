12e875ac7af1bf1d9b70c9d689642ac4779d82c8
tbonedev <tufytop@gmail.com>
2025-10-20 20:10:57 -0700

    change controller, add first route

--- server/src/coordinator/coordinator.controller.ts
+++ server/src/coordinator/coordinator.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ExecuteInputDto } from './dto/execute-input.dto';
+import { ExecutionResult } from './types/execution-result';
+import { CoordinatorService } from './coordinator.service';
 
-@Controller('tracing')
-export class TracingController {}
+@Controller('coordinator')
+export class CoordinatorController {
+  constructor(private readonly coordinatorService: CoordinatorService) {}
+
+  @Post('execute')
+  async execute(@Body() dto: ExecuteInputDto): Promise<ExecutionResult> {
+    return await this.coordinatorService.execute(dto);
+  }
+}
