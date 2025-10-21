fb0c24ea7863b728d6d0eb0f84e62aa504098ed3
tbonedev <tufytop@gmail.com>
2025-10-20 20:12:08 -0700

    upd imports

--- server/src/executor/executor.module.ts
+++ server/src/executor/executor.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ExecutorService } from './executor.service';
 
 @Module({
+  exports: [ExecutorService],
   providers: [ExecutorService],
 })
 export class ExecutorModule {}
