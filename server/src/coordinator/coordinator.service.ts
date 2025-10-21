fd16c96d74d252c9ed738b0f335169493c1ac2c2
tbonedev <tufytop@gmail.com>
2025-10-20 20:11:32 -0700

    implement execute

--- server/src/coordinator/coordinator.service.ts
+++ server/src/coordinator/coordinator.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ExecuteInputDto } from './dto/execute-input.dto';
+import { ExecutionResult } from './types/execution-result';
+import { ExecutorService } from 'src/executor/executor.service';
 
 @Injectable()
-export class TracingService {}
+export class CoordinatorService {
+  constructor(private readonly executorService: ExecutorService) {}
+
+  async execute(dto: ExecuteInputDto): Promise<ExecutionResult> {
+    //validate input
+
+    const trace = await this.executorService.execute(dto.code);
+    return {
+      success: true,
+      trace,
+      originalCode: dto.code,
+    };
+  }
+}
